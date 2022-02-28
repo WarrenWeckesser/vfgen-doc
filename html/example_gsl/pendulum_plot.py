c7f898dd5cfcc509ab04dd410f09d021ef1c9d05
warren <warren.weckesser@gmail.com>
2022-02-27 23:12:45 -0500

    Tweak gsl example plot legend, and fix plot filename in the HTML.

--- html/example_gsl/pendulum_plot.py
+++ html/example_gsl/pendulum_plot.py
@@ -12,13 +12,11 @@ plt.figure(1, figsize=(7.5, 4)).subplots_adjust(bottom=0.12)
 plt.xlabel('t')
 plt.grid(True)
 lw = 1
-plt.plot(t,theta, 'b', linewidth=lw)
-h = plt.plot(t, v, 'g', linewidth=lw)
-plt.plot(t,energy, 'r--', linewidth=lw)
-plt.legend((r'$\theta$', r'$v$', 'energy'), prop=FontProperties(size=16))
+plt.plot(t, theta, 'b', linewidth=lw, label=r'$\theta$')
+h = plt.plot(t, v, 'g', linewidth=lw, label=r'$v$')
+plt.plot(t, energy, 'r--', linewidth=lw, label='energy')
+plt.legend(framealpha=1, shadow=True, prop=FontProperties(size=16))
 plt.title('Damped Pendulum')
 plt.savefig('pendulum_plot.svg', dpi=200, transparent=True)
 
-# show()
-
-
+# plt.show()
