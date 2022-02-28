4b1ebffbbfa12309a3ae3688680d715a96d0d9bb
warren <warren.weckesser@gmail.com>
2022-02-28 12:13:50 -0500

    Update plots for the LSODA examples (plus a couple other tweaks).

--- html/example_lsoda/rp_plot.py
+++ html/example_lsoda/rp_plot.py
@@ -4,16 +4,15 @@ import matplotlib.pyplot as plt
 
 
 data = loadtxt('rp.dat')
-x = data[:,0]
+x = data[:, 0]
 
 plt.figure(1, figsize=(7.5, 6)).subplots_adjust(bottom=0.13, left=0.15)
 plt.grid(True)
-lw = 1
-plt.plot(x[:-1],x[1:], 'b.', linewidth=lw)
-plt.xlabel(r'$x_i$', size=18)
-plt.ylabel(r'$x_{i+1}$', size=18)
+plt.plot(x[:-1], x[1:], 'b.')
+plt.xlabel(r'$x_i$')
+plt.ylabel(r'$x_{i+1}$')
 plt.title(r'Rossler System, x Return Map')
 plt.axis('equal')
 plt.xlim(-12.5, -3)
 plt.ylim(-12.5, -3)
-plt.savefig('rp_plot.png', dpi=200, transparent=True)
+plt.savefig('rp_plot.svg', transparent=True)
