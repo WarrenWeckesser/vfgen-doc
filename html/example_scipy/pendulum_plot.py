180d491ee1fb9bdac82653e08d1a145498a6837f
Warren Weckesser <warren.weckesser@gmail.com>
2022-03-03 00:52:07 -0500

    Update scipy plot.

--- html/example_scipy/pendulum_plot.py
+++ html/example_scipy/pendulum_plot.py
@@ -1,23 +1,21 @@
 
 from numpy import loadtxt
 import matplotlib.pyplot as plt
-from matplotlib.font_manager import FontProperties
+
 
 t, theta, v, energy = loadtxt('pend.dat', unpack=True)
 
 plt.figure(1, figsize=(7.5, 4)).subplots_adjust(bottom=0.12)
+
+plt.plot(t, theta, label=r'$\theta$')
+plt.plot(t, v, '--', label=r'$v$')
+plt.plot(t, energy, '-.', label='energy')
+
+plt.title('Damped Pendulum')
 plt.xlabel('t')
 plt.grid(True)
-lw = 1
-plt.plot(t,theta, 'b', linewidth=lw)
-h = plt.plot(t,v, 'g', linewidth=lw)
-plt.plot(t,energy, 'r--', linewidth=lw)
-plt.ylim(-10, 11)
-plt.legend((r'$\theta$', r'$v$', 'energy'), prop=FontProperties(size=16))
-# plt.legend((r'$\theta$', r'$v$'), prop=FontProperties(size=16))
-plt.title('Damped Pendulum')
-plt.savefig('pendulum_plot.png', dpi=200, transparent=True)
+plt.legend(framealpha=1, shadow=True)
 
-# show()
+plt.savefig('pendulum_plot.svg', dpi=200, transparent=True)
 
-
+# plt.show()
