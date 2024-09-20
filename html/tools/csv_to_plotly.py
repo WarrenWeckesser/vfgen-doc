3a20391ff20de4e46093602336c0a89fd8a89d3a
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-20 13:18:34 -0400

    Use plotly in the delay2ode example plots.
    Also update csv_to_plotly.py to allow selecting a subset of the columns,
    and to provide alternate names to be used in the plot.

--- html/tools/csv_to_plotly.py
+++ html/tools/csv_to_plotly.py
@@ -1,32 +1,48 @@
 import sys
 
 
-styles = ['solid', 'dash', 'dot', 'dashdot']
+styles = ['solid', 'dot', 'dashdot']
 
 filename = sys.argv[1]
 with open(filename) as f:
     content = f.read()
+
+selected_headers = sys.argv[2:]
+display_name_map = {}
+selected_column_names = []
+for name in selected_headers:
+    if '=' in name:
+        column_name, display_name = name.split('=')
+    else:
+        column_name = display_name = name
+    selected_column_names.append(column_name)
+    display_name_map[column_name] = display_name
+
 lines = content.splitlines()
 headers = [field.strip() for field in lines[0].split(',')]
 data = [line.split(',') for line in lines[1:]]
 columns = list(zip(*data))
 
 for k in range(1, len(columns)):
-    print(f"const {headers[k]} = {{")
-    print("  x: ", end="")
-    values = ", ".join(columns[0])
-    print(f"[{values}],")
-    print("  y: ", end='')
-    values = ", ".join(columns[k])
-    print(f"[{values}],")
-    style = styles[(k - 1) % len(styles)]
-    print("  line: {")
-    print(f"    dash: '{style}'")
-    print("  },")
-    print(f"  name: '{headers[k]}'")
-    print("}")
+    if len(selected_column_names) == 0 or headers[k] in selected_column_names:
+        print(f"const {headers[k]} = {{")
+        print("  x: ", end="")
+        values = ", ".join(columns[0])
+        print(f"[{values}],")
+        print("  y: ", end='')
+        values = ", ".join(columns[k])
+        print(f"[{values}],")
+        style = styles[(k - 1) % len(styles)]
+        print("  line: {")
+        print(f"    dash: '{style}'")
+        print("  },")
+        print(f"  name: '{display_name_map[headers[k]]}'")
+        print("}")
 
 print("")
-names = ", ".join(headers[1:])
+if len(selected_column_names) == 0:
+    names = ", ".join(headers[1:])
+else:
+    names = ", ".join(selected_column_names)
 print(f"const data = [{names}]")
 print("export {data}")
